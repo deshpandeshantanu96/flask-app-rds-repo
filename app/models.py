bbb939a9d6a2faa8c2c3f2254c4ce2bfebbeb4f3
Shantanu <deshpandeshantanu96@gmail.com>
2025-05-13 17:30:05 +0530

    Removed the code

--- app/models.py
+++
@@ -1,9 +0,0 @@
-from app.database import db
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True)
-    email = db.Column(db.String(120), unique=True)
-
-    def __repr__(self):
-        return f'<User {self.username}>'
