8f6029a5e9037523f74ec2b3ef5d09c61e8a66d8
Shantanu <deshpandeshantanu96@gmail.com>
2025-05-15 00:10:09 +0530

    updated the .yml

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,14 +1,16 @@
 terraform {
-  required_version = ">= 1.0.0"
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.5"
+    }
   }
 }
 
 provider "aws" {
-  region = var.aws_region
+  region = "us-east-1"
 }
