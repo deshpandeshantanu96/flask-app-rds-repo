e92b9e3e3439e6de06302163c24964324dde44e8
Shantanu <deshpandeshantanu96@gmail.com>
2025-05-20 19:06:58 +0530

    Key pair creation code updated

--- customer-app/backend.py
+++ customer-app/backend.py
@@ -174,4 +174,4 @@ async def delete_user(index: int):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("backend:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("backend:app", host="0.0.0.0", port=8000, reload=True)
